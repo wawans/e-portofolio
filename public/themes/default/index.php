6c1dc4edd478628ece131d0cee3e06e01a1e77af
Wawan Setyawan <wawans.setyawan@gmail.com>
2016-05-18 06:05:21 +0700

    CSS tema default

--- public/themes/default/index.php
+++ public/themes/default/index.php
@@ -1,1 +1,20 @@
-Hello World!
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Aplikasi</title>
+    <meta content="app desc">
+    <meta name="geo.placename" content="Indonesia">
+    <meta name="language" content="id">
+    <meta name="author" content="Wawan Setyawan">
+    <meta name="email" content="Wawans.setyawan@gmail.com">
+    <link rel="author" href="https://plus.google.com/u/0/107005047094195947572"/>
+    <link rel="publisher" href="https://plus.google.com/u/0/107005047094195947572"/>
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>public/themes/default/css/style.css" media="screen">
+</head>
+<body>
+<h1>Hello World!</h1>
+</body>
+</html>

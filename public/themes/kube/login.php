0be9306a352445599b06a00ce3ea5441e5900cf9
Wawan Setyawan <wawans.setyawan@gmail.com>
2016-09-21 23:10:27 +0700

    Clean up working directory.

--- public/themes/kube/login.php
+++
@@ -1,8 +0,0 @@
-<h2>Login</h2>
-<hr/>
-<?php echo form_open('email/send'); ?>
-<?php echo form_label('Nama Pengguna', 'username'); ?>
-<?php echo form_input('username'); ?>
-<?php echo form_label('Kata Sandi', 'password'); ?>
-<?php echo form_password('password'); ?>
-<?php echo form_close(); ?>
